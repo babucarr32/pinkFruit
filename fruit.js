9aaf27b154981a50f0afa73b275a66dc8a622d29
babucarr32 <baboucarry@gmail.com>
2023-01-18 14:54:51 +0000

    Added change color effect with jS for all

--- fruit.js
+++ fruit.js
@@ -4,8 +4,28 @@ function changeColors(){
     const random = Math.floor(Math.random() * colors.length);
     setTimeout(function(){ 
         x = document.getElementById('fruit').style.color = colors[random];
+        // search ID
+        document.getElementById('fruit').style.color = colors[random];
+        document.getElementById('search').style.borderColor = colors[random];
+        document.getElementById('searchButton').style.backgroundColor = colors[random];
+        // round left buttons
+        document.getElementById('rightRoundButtonID').style.backgroundColor = colors[random];
+        document.getElementById('rightRoundButtonID').addEventListener('mouseover', function(){
+            document.getElementById('rightRoundButtonID').style.backgroundColor = colors[1];
+            console.log("mouse over")
+        })
+        document.getElementById('rightRoundButtonID2').style.backgroundColor = colors[random];
+        document.getElementById('welcomeText').style.color = colors[random];
+        document.getElementById('fruitTitleID').style.backgroundColor = colors[random];
+        document.getElementById('fruitTitleID3').style.backgroundColor = colors[random];
+        document.getElementById('fruitTitleID4').style.backgroundColor = colors[random];
+        document.querySelector('body').style.backgroundImage = `linear-gradient(white, white, ${colors[random]})`; 
+        // add icons
+        document.getElementById('more').style.color = colors[random];
+        document.getElementById('more3').style.color = colors[random];
+        document.getElementById('more4').style.color = colors[random];
         console.log(x)
         changeColors()
-    }, 1000);
+    }, 3000);
     
 }
